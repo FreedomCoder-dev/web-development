0553dc1306e416b0e7e8b8c076f3457399391008
Freedomcoder <admin@freedomcoder.ru>
2021-03-01 10:35:12 +0300

    Commit of lw2

---
+++ lw2/2.pas
@@ -0,0 +1,12 @@
+PROGRAM PrintHello(INPUT, OUTPUT);
+USES DOS;
+BEGIN {PrintHello}
+  WRITELN('Content-Type: text/html');
+  WRITELN;
+  WRITELN('<h1>OH MY PASCAL</h1>');
+  WRITELN('<p> REQUEST_METHOD: ', GetEnv('REQUEST_METHOD'), '</p>');
+  WRITELN('<p> QUERY_STRING: ', GetEnv('QUERY_STRING'), '</p>');
+  WRITELN('<p> CONTENT_LENGTH: ', GetEnv('CONTENT_LENGTH'), '</p>');
+  WRITELN('<p> HTTP_USER_AGENT: ', GetEnv('HTTP_USER_AGENT'), '</p>');
+  WRITELN('<p> HTTP_HOST: ', GetEnv('HTTP_HOST'), '</p>');
+END. {PrintHello}
